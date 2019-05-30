e67b343087e6b80e8e1716dd5e89db0504d0efa0
William Florez <williamflorezmaestre@gmail.com>
2019-05-30 13:14:36 -0500

    Add TD3 heads

--- neural_network_AC_model.py
+++ neural_network_AC_model.py
@@ -53,32 +53,66 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=units1, fc2_units=units2):
+    def __init__(self, state_size, action_size, seed, fc1_units=units1, fc2_units=units2):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
+            fc1_units (int): Number of nodes in the first hidden layer
             fc2_units (int): Number of nodes in the second hidden layer
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        
+        # Q1 architecture
+        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
+        
+        
+        # Q2 architecture
+        
+        self.fc4 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc5 = nn.Linear(fc1_units, fc2_units)
+        self.fc6 = nn.Linear(fc2_units, 1)
+        
+        
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        
+        self.fc4.weight.data.uniform_(*hidden_init(self.fc4))
+        self.fc5.weight.data.uniform_(*hidden_init(self.fc5))
+        self.fc6.weight.data.uniform_(-3e-3, 3e-3)
 
 
     def forward(self, state, action): #concatenate the action -value
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))#F.leaky_relu
-        x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+
+        xu = torch.cat([state, action], 1)
+
+        x1 = F.relu(self.fc1(xu))
+        x1 = F.relu(self.fc2(x1))
+        x1 = self.fc3(x1)
+
+        x2 = F.relu(self.fc4(xu))
+        x2 = F.relu(self.fc5(x2))
+        x2 = self.fc6(x2)
+        
+        return x1, x2
+        
+    def Q1(self, state, action):
+    
+        xu = torch.cat([state, action], 1)
+
+        x1 = F.relu(self.fc1(xu))
+        x1 = F.relu(self.fc2(x1))
+        x1 = self.fc3(x1)
+        
+        return x1 
+   
